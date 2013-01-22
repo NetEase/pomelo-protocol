b71be344a375cc345c821299305c9043c2ec172f
yaopiaohai <yaopiaohai@corp.netease.com>
2013-01-22 21:30:34 +0800

    misc:fix new version define

--- test/protocol-test.js
+++ test/protocol-test.js
@@ -6,13 +6,16 @@ console.log(protocol);
 describe(' encode and decode Test ',function() {
 	it(' normal test',function() {
 		var flag = 3;
-		var bt = new Buffer(2);
+		var bt = new Buffer(3);
 		bt[0] = 0x1;
 		bt[1] = 0x1;
+		bt[2] = 0x1;
 		var buf = protocol.head.encode(flag,bt);
 		console.log(buf);
+		buf[bt.length+2].should.equal(1);
 		var m = protocol.head.decode(buf);
 		console.log(m);
+		m.flag.should.equal(3);
 		buf = protocol.body.encode(1,0,protocol.strencode('move'),protocol.strencode('Hello world'))
 		var _m = protocol.body.decode(buf);
 		console.log(protocol.strdecode(_m.route));
